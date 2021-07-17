e5eafc0bd44532ccaa5f710b73659118c293714a
Anamika <avikajha@Avikas-iMac.local>
2021-07-17 12:31:47 -0700

    added high score functionality

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -9,8 +9,15 @@ Quiz.prototype.getQuestionIndex = function() {
 }
  
 Quiz.prototype.guess = function(answer) {
+    var element = document.getElementById("success");
+    element.innerHTML = "Wrong Answer";
     if(this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
+        element.innerHTML = "Correct Answer";
+    }
+    else{
+        // Reduce time for wrong answer.
+        remainingSeconds = remainingSeconds - 15;
     }
  
     this.questionIndex++;
@@ -31,10 +38,13 @@ Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
  
- 
+function start() {
+    document.querySelector('#time').textContent = 75;
+    startTimer(75, document.querySelector('#time'));
+    populate();
+} 
 function populate() {
-    document.querySelector('#time').textContent = 15;
-    startTimer(15, document.querySelector('#time'));
+  
     if(quiz.isEnded()) {
         showScores();
     }
@@ -63,12 +73,12 @@ function guess(id, guess) {
     var button = document.getElementById(id);
     button.onclick = function() {
         quiz.guess(guess);
-        clearTimer();
+        //clearTimer();
         populate();
     }
 };
  
- 
+
 function showProgress() {
     var currentQuestionNumber = quiz.questionIndex + 1;
     var element = document.getElementById("progress");
@@ -80,35 +90,37 @@ function showScores() {
     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
+    var highScores = document.getElementById("end-screen");
+    highScores.style.display ="block";
+    var element = document.getElementById("scores");
+    element.style.display ="none";
     
 };
 
-function highScores() {
+/* function highScores() {
     var gameOverHTML = "<h1>High Scores</h1>";
     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
-};
+}; */
  
 // create questions here
 var questions = [
-    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
-    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-    new Question("What are you learning in this bootcamp", ["Web Design", "Graphic Design", "Development", "All"], "All")
+    new Question("Commonly used data types DO NOT include:", ["strings", "booleans", "alerts", "numbers"], "alerts"),
+    new Question("The condition in an if / else statement is enclosed within ____.", ["quotes", "curly brackets", "parentheses", "square brackets"], "parentheses"),
+    new Question("Arrays in JavaScript can be used to store ____.", ["numbers and strings",
+    "other arrays","booleans","all of the above"], "all of the above"),
+    new Question("String values must be enclosed within ____ when being assigned to variables.", ["commas", "curly brackets", "quotes", "parentheses"], "quotes"),
+    new Question("A very useful tool used during development and debugging for printing content to the debugger is:", ["JavaScript", "terminal / bash", "for loops", "console.log"], "console.log")
 ];
-var timerFunction;
+
+var remainingSeconds = 75;
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    timerFunction = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
+    setInterval(function () {
+        remainingSeconds--;
+        display.textContent = remainingSeconds;
 
         if (--timer < 0) {
             timer = duration;
@@ -116,9 +128,67 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-function clearTimer(){
+function saveHighscore() {
+    var element = document.getElementById("quiz");
+    element.style.display = "none";
+    // get value of input box
+    var initials = document.querySelector("#initials").value.trim();
+  
+    if (initials !== "") {
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      // format new score object for current user
+      var newScore = {
+        score: quiz.score,
+        initials: initials
+      };
+  
+      // save to localstorage
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+      var element = document.getElementById("end-screen");
+      element.style.display = "none";
+      printHighScores();
+
+    }
+  }
+
+  function printHighScores() {
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+    // sort highscores by score property in descending order
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // create li tag for each high score
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+      // display on page
+      var olEl = document.getElementById("highscoreslist");
+      olEl.appendChild(liTag);
+    });
+    var highScores = document.getElementById("highscoreslist");
+    highScores.style.display ="block";
+
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+
+  function goback() {
+    window.location.reload();
+  }
+
+/* function clearTimer(){
     clearInterval(timerFunction);
-}
+} */
  
 // create quiz
 var quiz = new Quiz(questions);
